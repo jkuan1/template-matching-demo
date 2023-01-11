5f8a2894e570d88a4daf3d95e5b3f879e473426f
Justin Kuan <justin.kuan@ttt.studio>
2023-01-11 15:10:09 -0800

    enhanced the basic matrix manipulation page

--- pages/3_Very_Basic_Matrix_Manipulation.py
+++ pages/3_Very_Basic_Matrix_Manipulation.py
@@ -1,7 +1,5 @@
 import streamlit as st
 from utils import show_code
-from skimage import data
-
 
 st.markdown(
     """
@@ -9,26 +7,35 @@ st.markdown(
     """
 )
 
-cat_dims = data.chelsea().shape
-
-# Interactive elements
-
-red = st.number_input("RED VALUE", 0, 255, 255)
-green = st.number_input("GREEN VALUE", 0, 255, 0)
-blue = st.number_input("BLUE VALUE", 0, 255, 0)
-
-filter_size = st.slider("Filter size", 5, 200, 5)
-
-x_coord = st.slider("Move the filter left to right",
-                    0, cat_dims[1] - filter_size, 0)
-y_coord = st.slider("Move the filter top to bottom",
-                    0, cat_dims[0] - filter_size, 0)
-
 
 def render_cat():
+    from skimage import data
+    import numpy as np
+
     cat = data.chelsea()
+    cat_dims = cat.shape
+
+    # Interactive elements
+
+    red = st.number_input("RED VALUE", 0, 255, 255)
+    green = st.number_input("GREEN VALUE", 0, 255, 0)
+    blue = st.number_input("BLUE VALUE", 0, 255, 0)
+
+    filter_size = st.slider("Filter size", 5, 200, 5)
+
+    filter = np.full((filter_size, filter_size, 3), [red, green, blue])
+
+    st.write("The filter that has been configured:")
+    st.image(filter)
+
+    x_coord = st.slider("Move the filter left to right",
+                        0, cat_dims[1] - filter_size, 0)
+    y_coord = st.slider("Move the filter top to bottom",
+                        0, cat_dims[0] - filter_size, 0)
+
     cat[y_coord:y_coord + filter_size,
         x_coord:x_coord + filter_size, :] = [red, green, blue]
+
     return cat
 
 
