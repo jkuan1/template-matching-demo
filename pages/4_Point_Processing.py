8964ff3732b3effcf97825817da0b3d07bae9c74
Justin Kuan <justin.kuan@ttt.studio>
2023-01-11 16:11:10 -0800

    Created more examples of point processing

--- pages/4_Point_Processing.py
+++ pages/4_Point_Processing.py
@@ -2,6 +2,7 @@ import streamlit as st
 from utils import show_code
 from PIL import Image
 import numpy as np
+import math
 
 st.markdown(
     """
@@ -29,19 +30,21 @@ st.markdown(
 )
 
 option = st.selectbox("Type of Point Transformation",
-                      ("None (Original)", "Invert", "Lower Contrast", "Increase Contrast"))
+                      ("None (Original)", "Invert", "Lower Contrast", "Increase Contrast", "Constant Darken", "Constant Lighten", "Sinusoidal Pattern"))
+
+
+image_width = 600
+image_transform = None
 
 match option:
     case "None (Original)":
         st.write("Forumla: I'(X,Y) = I(X,Y)")
 
-        def get_original_image():
-
-            roxy = Image.open("./pictures/roxy.jpg")  # Get image
+        def get_image():
+            roxy = Image.open("./pictures/roxy.jpg")
             return roxy
 
-        show_code(get_original_image)
-        st.image(get_original_image())
+        image_transform = get_image
 
     case "Invert":
         st.write("Forumla: I'(X,Y) = 255 - I(X,Y)")
@@ -58,8 +61,7 @@ match option:
 
             return roxy_inverted
 
-        show_code(get_image)
-        st.image(get_image())
+        image_transform = get_image
 
     case "Lower Contrast":
         st.write("Forumla: I'(X,Y) = I(X,Y) * 0.5")
@@ -69,14 +71,13 @@ match option:
             roxy = Image.open("./pictures/roxy.jpg")
 
             roxy_matrix = np.asarray(roxy, np.float32)
-            roxy_darkened = roxy_matrix * 0.5  # contrast operation
+            roxy_lower_contrast = roxy_matrix * 0.5  # contrast operation
 
-            roxy_darkened = np.clip(roxy_darkened, 0, 255)
-            roxy_darkened = roxy_darkened.astype('uint8')
-            return roxy_darkened
+            roxy_lower_contrast = np.clip(roxy_lower_contrast, 0, 255)
+            roxy_lower_contrast = roxy_lower_contrast.astype('uint8')
+            return roxy_lower_contrast
 
-        show_code(get_image)
-        st.image(get_image())
+        image_transform = get_image
 
     case "Increase Contrast":
         st.write("Forumla: I'(X,Y) = I(X,Y) * 1.5")
@@ -86,11 +87,66 @@ match option:
             roxy = Image.open("./pictures/roxy.jpg")
 
             roxy_matrix = np.asarray(roxy, np.float32)
-            roxy_lightened = roxy_matrix * 1.5  # contrast operation
+            roxy_increase_contrast = roxy_matrix * 1.5  # contrast operation
 
-            roxy_lightened = np.clip(roxy_lightened, 0, 255)
-            roxy_lightened = roxy_lightened.astype('uint8')
-            return roxy_lightened
+            roxy_increase_contrast = np.clip(roxy_increase_contrast, 0, 255)
+            roxy_increase_contrast = roxy_increase_contrast.astype('uint8')
+            return roxy_increase_contrast
 
-        show_code(get_image)
-        st.image(get_image())
+        image_transform = get_image
+
+    case "Constant Darken":
+        st.write("Forumla: I'(X,Y) = I(X,Y) - 100")
+        st.write("Note: It can be any value! Not just 100!")
+
+        def get_image():
+            roxy = Image.open("./pictures/roxy.jpg")
+
+            roxy_matrix = np.asarray(roxy, np.float32)
+            roxy_darken = roxy_matrix - 100
+
+            roxy_darken = np.clip(roxy_darken, 0, 255)
+            roxy_darken = roxy_darken.astype('uint8')
+            return roxy_darken
+
+        image_transform = get_image
+
+    case "Constant Lighten":
+        st.write("Forumla: I'(X,Y) = I(X,Y) + 100")
+        st.write("Note: It can be any value! Not just 100!")
+
+        def get_image():
+            roxy = Image.open("./pictures/roxy.jpg")
+
+            roxy_matrix = np.asarray(roxy, np.float32)
+            roxy_lighten = roxy_matrix + 100
+
+            roxy_lighten = np.clip(roxy_lighten, 0, 255)
+            roxy_lighten = roxy_lighten.astype('uint8')
+            return roxy_lighten
+
+        image_transform = get_image
+
+    case "Sinusoidal Pattern":
+        st.write("Forumla: I'(X,Y) = (sin(X * 0.01) + 1) / 2 * I(X,Y)")
+
+        def get_image():
+            roxy = Image.open("./pictures/roxy.jpg")
+
+            roxy_matrix = np.asarray(roxy, np.float32)
+            sine_vector = (np.sin(
+                [0.01 * x for x in range(0, roxy_matrix.shape[1])]) + 1) / 2
+            roxy_sine = np.zeros(roxy_matrix.shape)
+
+            for x in range(0, roxy_matrix.shape[1]):
+                roxy_sine[:, x, :] = roxy_matrix[:, x, :] * sine_vector[x]
+
+            roxy_sine = np.clip(roxy_sine, 0, 255)
+            roxy_sine = roxy_sine.astype('uint8')
+            return roxy_sine
+
+        image_transform = get_image
+
+
+st.image(image_transform(), width=image_width)
+show_code(image_transform)
